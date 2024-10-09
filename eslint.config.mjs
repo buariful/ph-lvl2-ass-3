2983abc2c41814a621ce94b400f90c5f9f00c58b
ariful <arifulislam321054@gmail.com>
2024-10-09 14:48:39 +0600

    prettier setup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
+import prettierConfig from "eslint-config-prettier";
 
 export default [
   { files: ["**/*.{js,mjs,cjs,ts}"] },
@@ -10,6 +11,7 @@ export default [
   },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
+  prettierConfig,
   {
     ignores: ["dist", "node_modules"],
     rules: {
